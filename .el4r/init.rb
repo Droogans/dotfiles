627c2f860296e62f001380ca54fd1d4d9e86e592
Droogans <droogans@gmail.com>
2014-09-17 11:15:25 -0500

    Remove xiki Ruby code

--- .el4r/init.rb
+++
@@ -1,6 +0,0 @@
-$LOAD_PATH << "/Users/andr6283/code/rb/xiki/lib/"
-# $LOAD_PATH << "/projects/xiki/lib/"
-require 'xiki'
-Xiki.init
-Xiki::KeyShortcuts.keys   # Use default key shortcuts
-# Xiki::Themes.use "Default"  # Use xiki theme
