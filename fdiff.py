9522853f90a9cd1300fa7afe44d6dc80a2b76ed6
captain-kark <andrew.yurisich@nbx.com>
2020-03-20 13:50:36 +0100

    Handle diff --stat HEAD not liking revert/reset activity

--- fdiff.py
+++ fdiff.py
@@ -12,16 +12,35 @@ st_longest = max([len(p[1]) for p in st_parts])
 if tracked == ['']:
     tracked = []
 
+if untracked == ['']:
+    untracked = []
+
 split_tracked = []
 pad_tracked = 0
 if tracked:
     split_tracked = [t.split(' ')[1] for t in tracked]
     pad_tracked = max(len(t) for t in split_tracked)
 
-pad_untracked = max(len(u) for u in untracked)
+pad_untracked = 0
+if untracked:
+    pad_untracked = max(len(u) for u in untracked)
+
 pad_counts = max(pad_tracked, pad_untracked)
 diff_stats = split_tracked + [u or '\x1b[33m{}\x1b[m'.format('?' * pad_counts) for u in untracked]
 diff_lines = [' '.join(t.split(' ')[2:]) for t in tracked] + ['\x1b[33m+~\x1b[m' for u in untracked]
-pad_st = ['{:<{}} | {:>{}} {}'.format(s, (st_longest + len(st_parts[i][0])), diff_stats[i], pad_counts, diff_lines[i]) for i, s in enumerate(st)]
+pad_st = []
+for i, s in enumerate(st):
+    try:
+        diff_stat = diff_stats[i]
+    except IndexError:
+        # happens when reverting/applying through changesets and a +n/-n result is identical both ways
+        diff_stat = '0'
+
+    try:
+        diff_line = diff_lines[i]
+    except IndexError:
+        diff_line = '\x1b[33m±\x1b[m'
+
+    pad_st.append('{:<{}} | {:>{}} {}'.format(s, (st_longest + len(s[0])), diff_stat, pad_counts, diff_line))
 print('\n'.join(pad_st))
 sys.exit(0)
